0b7210b971af1f85973ff72eeac5a9b8983aebdc
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-20 19:35:01 +0530

    Update mirror-distance-of-an-integer.java

--- 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
@@ -17,3 +17,22 @@ class Solution {
         
     }
 }
+
+//Another way using function
+
+
+class Solution {
+    public int mirrorDistance(int n) {
+        int copy = n;
+        int digit = 0;
+        int rev = 0;
+        while(n>0){
+            digit = n % 10;
+            rev = rev*10+digit;
+            n = n/10;
+        }
+        return Math.abs(copy-rev);
+
+        
+    }
+}
