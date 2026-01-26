82a511a5e8b433a5bf5dc062f1db733b9a65e134
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-26 19:47:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (55.82%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -1,50 +1,14 @@
-/*
-if(num == 0){
-    return true;
-}
-else if(num % 10 == 0){
-    return false;
-}
-else{
-    return n*2;
-}
-*/
-
-
-
-//Other approach solution
-/*
-
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        int digit = 0;
-        int reversed1 = 0;
-        int reversed2 = 0;
-        if(num < 0){
-            return false;
-        }
-        else if(num == 0){
+        if(num == 0){
             return true;
         }
-        else{
-            for(int i = num; i>0; i = i /10){
-                digit = i % 10;
-                reversed1 = reversed1*10 + digit;
-
-            }
-            for(int j = reversed1; j>0; j = j / 10){
-                digit = j % 10;
-                reversed2 = reversed2*10 + digit;
-            }
-        }
-        if(num == reversed2){
-            return true;
-        }
-        else{
+        else if(num % 10 == 0){
             return false;
         }
-
+        else {
+            return true;
+        }
         
     }
 }
-*/
