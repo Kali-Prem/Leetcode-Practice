2031a1fea76bf11fc3a0d503c05abb0310199395
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-04-09 16:33:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (72.12%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isHappy(int n) {
+        // int copy = n;
+        int temp = n;
+        if(n<0) return false;
+        else{
+            while(temp > 1 && temp != 4){ 
+                //check also this, Because all unhappy numbers eventually reach 4. and without checking this the loops goes into the infinite loops
+                int sum = 0;
+                int copy = temp;
+                while(copy > 0){
+                    int digit = copy % 10;
+                    sum = sum + digit*digit;
+                     copy = copy / 10;
+                }
+                temp = sum;
+            }
+
+        }
+        if(temp == 1) return true;
+        else return false;
+       
+        
+       
+
+
+    //    =====================
+        // int copy = n;
+        // //int temp = n;
+        // if(n < 0){
+        //     return false;
+        // }
+        // else{
+        //     while(copy > 1){
+        //         int temp = copy;
+        //         int sqr = 0;
+        //         while(temp > 1){
+        //         int digit = temp % 10;
+        //         sqr = sqr + digit*digit;
+        //         temp = temp / 10;
+        //         }
+        //         copy = sqr;
+        //     }
+            
+        // }
+        // if(copy == 1){
+        //         return true;
+        // }
+        // else{
+        //     return false;
+        // }
+    }
+}
