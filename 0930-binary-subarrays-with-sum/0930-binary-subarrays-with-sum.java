aeb141caf3168661d25e135baea30edec7ddef8f
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-04-29 20:02:19 +0530

    Time: 2671 ms (5.12%), Space: 51 MB (54.78%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        int n = nums.length;
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum = sum + nums[j];
+                if(sum == goal){
+                    count++;
+                }
+            }
+
+        }
+        return count;
+    }
+}
+
+
+// Total subarrays :-  n(n+1)/2
+// SubArray is the contiguous part of an array
+// 
+
+//Hint :- Fix the first index element and then compare to the goal if equal then count++ otherwise expand the element from fist index to next tilll n; then after n, we increment the first index to 1 and then 1 to n; 
