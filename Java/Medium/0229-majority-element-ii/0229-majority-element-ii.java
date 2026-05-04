6c29d5e674913be92967597968f946b006ddb805
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-05-04 19:54:30 +0530

    Time: 18 ms (14.27%), Space: 52.4 MB (64.21%) - LeetHub

--- Java/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ Java/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,14 +4,8 @@ class Solution {
         HashMap<Integer, Integer> hm = new HashMap<>();
 
         // put the all values in the map
-        for(int i = 0; i<n; i++){
-            int x = nums[i];
-            if(hm.containsKey(x)){
-                hm.put(x,hm.getOrDefault(x,0)+1);
-            }
-            else{
-                hm.put(x,1);
-            }
+        for(int ele:nums){
+            hm.put(ele,hm.getOrDefault(ele,0)+1);
         }
         List<Integer> result = new ArrayList<>();
         // CHecking the leement
