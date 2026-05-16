8bba269ea038efc242e5fce8975f04226d264c41
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-05-16 12:22:10 +0530

    Time: 1 ms (98.53%), Space: 47 MB (95.86%) - LeetHub

---
+++ Java/Easy/0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        // Prefix sum
+        int preSum[] = new int[n];
+        int sum1 = 0;
+        for(int i = 0; i<n; i++){
+            sum1 = sum1 + nums[i];
+            preSum[i] = sum1;
+        }
+        // Suffix sum
+        int suffSum[] = new int[n];
+        int sum2 = 0;
+        for(int i = n-1; i >= 0; i--){
+            sum2 = sum2 + nums[i];
+            suffSum[i] = sum2;
+        }
+        // Check the equal elmeent in both array
+        for(int i = 0; i<n; i++){
+            if(preSum[i] == suffSum[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
