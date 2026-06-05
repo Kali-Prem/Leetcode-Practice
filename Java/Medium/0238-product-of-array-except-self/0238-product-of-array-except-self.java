3520649bfd9852de643bf4d1b8b3de6e6d7b6c1a
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-06-05 19:47:57 +0530

    Time: 4 ms (7.22%), Space: 64.9 MB (85.84%) - LeetHub

--- Java/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ Java/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,12 +6,12 @@ class Solution {
         prePro[0] = 1;
         suffPro[n - 1] = 1;
         // for prePro
-        for (int i = 1; i < n; i++) {
-            prePro[i] = nums[i-1] * prePro[i-1];
+        for (int i = 0; i < n - 1; i++) {
+            prePro[i + 1] = nums[i] * prePro[i];
         }
         // for suffPRO
-        for (int i = n - 2; i >= 0; i--) {
-            suffPro[i] = suffPro[i+1] * nums[i+1];
+        for (int i = n - 1; i > 0; i--) {
+            suffPro[i - 1] = suffPro[i] * nums[i];
         }
         // Product of both
         int pro[] = new int[n];
