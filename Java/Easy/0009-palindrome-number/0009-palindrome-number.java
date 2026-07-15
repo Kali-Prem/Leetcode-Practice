e6a5307def256c118f33dfa7d19cd252dfc7dda5
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-07-15 19:33:53 +0530

    Time: 5 ms (80.69%), Space: 45.8 MB (87.56%) - LeetHub

--- Java/Easy/0009-palindrome-number/0009-palindrome-number.java
+++ Java/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int rev = 0;
-        int temp = x;
-        int k = reverse(rev,x);
+        boolean k = reverse(rev, x, x);
         if(x < 0) return false;
-        if(k == temp){
+        if(k){
             return true;
         }
         else return false;
     }
-    public static int reverse(int rev, int x){
-        if(x == 0) return rev;
-        return reverse(rev*10+x%10, x/10);
+    public static boolean reverse(int rev, int x, int temp){
+        if(x == 0) return rev==temp;
+        return reverse(rev*10+x%10, x/10, temp);
     }
 }
