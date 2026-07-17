e9b8d49c39222d365d0c950562962a6c0435ac19
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-07-17 19:46:29 +0530

    Time: 1 ms (99.88%), Space: 44.4 MB (55.61%) - LeetHub

--- Java/Easy/3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
+++ Java/Easy/3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -2,9 +2,11 @@ class Solution {
     public String trimTrailingVowels(String s) {
         int n = s.length();
         int count = 0;
+        // String vowels = "aeiou";
         
         for(int i = n-1; i >= 0; i--){
             char ch = s.charAt(i);
+            // if(vowels.indexOf(ch) != -1) count++;
             if(ch == 'a' || ch == 'e' || ch == 'i' ||
                 ch == 'o' || ch == 'u' ){
                 count++;
